d2a25a9d4cfdeaed6f17fa9e5fea16cfb3599689
Terry <478409784@qq.com>
2017-04-06 17:04:05 +0800

    service

--- src/components/tradingService/ProductTradingForm.vue
+++ src/components/tradingService/ProductTradingForm.vue
@@ -1,4 +1,169 @@
 <template>
+  <div class="am-u-sm-12 am-u-md-12 am-u-lg-12" >
+    <div class="widget am-cf">
+      <div class="widget-body am-fr">
+        <form class="am-form tpl-form-border-form tpl-form-border-br" data-am-validator :id="id">
+          <fieldset>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>商品分类
+              </label>
+              <div class="am-u-sm-3 am-u-end input-field">
+                <select2 required v-model="formData.categoryId" :options="category" >
+                  <option value="">请选择</option>
+                </select2>
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>商品名称
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text"  class="am-form-field" placeholder="请输入商品名称" required v-model="formData.productName" >
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>商品价格
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text"  class="am-form-field" placeholder="请输入商品价格" required v-model="formData.price" >元
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>单位
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text"  class="am-form-field" placeholder="请输入商品单位" required v-model="formData.unit" >
+              </div>
+            </div>
+
+            <div class="am-form-group am-u-sm-9 am-u-sm-offset-3 am-u-end">
+              <span class="am-text-danger am-margin-right-xs am-text-xs"></span>商品描述
+              <div class="am-tabs" data-am-tabs>
+                <div class="am-tabs-bd">
+                  <div class="am-tab-panel am-fade am-in am-active" id="tab1">
+                    <editor v-model="formData.content"></editor>
+                  </div>
+                </div>
+              </div>
+            </div>
 
 
+            <div class="am-form-group">
+              <div class="am-u-sm-9 am-u-sm-push-3">
+                <button type="submit" class="am-btn am-btn-primary">保存</button>
+                <a href="javascript:void(0)" data-am-modal-close><button class="am-btn am-btn-primary">取消</button></a>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  import io from '../../lib/io'
+  import util from '../../lib/util'
+  export default{
+    data(){
+      return{
+        formData:{
+          productName:'',
+          content:'',
+          price:'',
+          unit:'',
+          categoryId:'',
+        },
+        category:[],
+      }
+    },
+    created:function(){
+      this.loadCategoryData()
+    },
+    computed:{
+
+    },
+    mounted:function(){
+      var _this = this ;
+      $('#' + this.id ).validator({
+        validate:function(validity){
+
+        },
+        onValid: function(validity) {
+          $(validity.field).closest('.input-field').find('.am-alert').hide();
+        },
+        onInValid: function(validity) {
+          var $field = $(validity.field);
+          var $group = $field.closest('.input-field');
+          var $alert = $group.find('.am-alert');
+          // 使用自定义的提示信息 或 插件内置的提示信息
+          var msg = $field.data('validationMessage') || this.getValidationMessage(validity);
+
+          if (!$alert.length) {
+            $alert = $('<div class="am-alert am-alert-danger"></div>').hide().
+            appendTo($group);
+          }
+
+          $alert.html(msg).show();
+        },
+        submit:function(e){
+          e.preventDefault();
+          var $submitBtn = $('button[type=submit]',e.target);
+          $submitBtn.attr("disabled" ,"disabled" )
+          _this.$showLoading()
+          var formValidity = this.isFormValid();
+          var complete = function(){
+            _this.$hiddenLoading()
+            $submitBtn.removeAttr("disabled" ,"disabled" )
+          }
+          if(formValidity){
+            _this.save(complete);
+          }else{
+            complete.call()
+          }
+        }
+      });
+    },
+
+    methods:{
+      save:function(complete){
+        var _this = this
+        var data = _this.formData
+        io.post(io.apiAdminSaveServiceProduct,data,
+          function(ret){
+            complete.call()
+            if(ret.success){
+              _this.$toast('OK')
+            }else{
+              _this.$alert(ret.desc)
+            }
+
+          },
+          function(){
+            complete.call()
+            _this.$alert('请求服务器失败')
+          })
+        _this.$emit('addSuccess')
+      },
+      loadCategoryData:function () {
+        var _this = this
+        io.post(io.apiAdminGetAllCategoryDetail, {}, function (ret) {
+          if (ret.success) {
+            _this.category = ret.data.map(function (item) {
+              return {value: item.categoryId, text: item.name}
+            })
+          } else {
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+    }
+  }
+</script>
--- src/components/tradingService/ProductTradingList.vue
+++ src/components/tradingService/ProductTradingList.vue
@@ -1,3 +1,168 @@
 <template>
-  <h1>商品管理</h1>
+  <div >
+    <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
+      <div class="widget am-cf">
+        <div class="widget-head am-cf">
+          <div class="widget-title  am-cf">商品列表</div>
+        </div>
+        <div class="widget-body  am-fr">
+
+          <div class="am-u-sm-12 am-form ">
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group tpl-table-list-select">
+                <div class="am-form-group">
+                  <select2  required v-model="query.categoryId" :options="category">
+                    <option value="">商品分类</option>
+                  </select2>
+                </div>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <input type="text" class="am-input-lg" name="productName" v-model="query.productName" placeholder="请输入商品名称"/>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <button type="button" class="am-btn am-btn-default am-btn-success am-btn-lg"
+                        @click="search" ><span class="am-icon-search"></span>查询
+                </button>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <button type="button" class="am-btn am-btn-default am-btn-success" @click="addServiceProduct()"><span  class="am-icon-plus"></span>新增</button>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="am-u-sm-12 am-scrollable-horizontal">
+            <table width="100%" class="am-table am-table-bordered am-table-compact am-table-striped am-text-nowrap">
+              <thead>
+              <tr>
+                <th>序号</th>
+                <th>商品名称</th>
+                <th>商品分类</th>
+                <th>单价</th>
+                <th>单位</th>
+                <th>更新人</th>
+                <th>更新时间</th>
+              </tr>
+              </thead>
+              <tbody>
+
+              <tr v-for="(item,index) in tableData" :key="item.productId">
+                <td>{{index+1}}</td>
+                <td>{{item.productName}}</td>
+                <td>{{item.categoryName}}</td>
+                <td>{{item.price}}</td>
+                <td>{{item.unit}}</td>
+                <td>{{item.username}}</td>
+                <td>{{item.updateTime | formatDate}}</td>
+              </tr>
+
+              </tbody>
+            </table>
+          </div>
+          <div class="am-u-lg-12 am-cf">
+
+            <div class="am-fr">
+              <pagination v-bind:total="total" v-bind:pageNo="pageNo" v-bind:pageSize="pageSize" @paging="loadTableData" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <window ref="add_Serviceproduct" title="新建商品">
+      <add-ServiceProduct @addSuccess="$refs.add_Serviceproduct.close()"></add-ServiceProduct>
+    </window>
+
+
+  </div>
+
+
+  </div>
 </template>
+
+<script>
+  import io from '../../lib/io'
+
+  import Pagination from '../base/Pagination'
+  import ProductTradingForm from '../tradingService/ProductTradingForm'
+
+  export default{
+    data:function(){
+      return {
+        tableData:[],
+        total:0,
+        pageSize:10,
+        pageNo:1,
+        query:{
+          categoryId:'',
+          productName:'',
+        },
+        category:[],
+        searchConfig:{}
+      }
+    },
+    components: {
+      Pagination,
+      'add-ServiceProduct':ProductTradingForm
+    },
+    mounted:function(){
+      $(window).smoothScroll()
+    },
+    created:function(){
+      this.loadTableData(this.pageNo);
+      this.loadCategoryData();
+    },
+    computed:{
+    },
+    methods:{
+      search:function(){
+        this.loadTableData()
+      },
+      loadCategoryData:function () {
+        var _this = this
+        io.post(io.apiAdminGetAllCategoryDetail, {}, function (ret) {
+          if (ret.success) {
+            _this.category = ret.data.map(function (item) {
+              return {value: item.categoryId, text: item.name}
+            })
+          } else {
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+      loadTableData:function(pageNo){
+        var _this = this
+        _this.pageNo = pageNo || _this.pageNo || 1
+        io.post(io.apiAdminServiceProductList,$.extend({
+          pageNo:_this.pageNo,
+          pageSize:_this.pageSize
+        },_this.query),function(ret){
+          if(ret.success){
+            _this.total = ret.data.total
+            _this.tableData = ret.data.list
+          }else{
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+      addServiceProduct:function () {
+        //弹窗
+        var _this = this;
+        _this.$refs.add_Serviceproduct.show({
+          width : 1000,
+          height: 500
+        });
+      }
+    }
+  }
+</script>
