5370a75f3d73c7b8e5c2c432c23c1b9ad4d5a49c
sam <sam@hboxs.com>
2017-04-18 19:16:47 +0800

    rent

---
+++ src/components/tradingService/ServiceRentForm.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
+    <div class="widget am-cf">
+      <div class="widget-body am-fr">
+        <div class="widget-function am-fr">
+          <button type="button" class="am-btn am-btn-default" @click="$router.go(-1)">返回</button>
+        </div>
+        <form class="am-form tpl-form-border-form tpl-form-border-br" data-am-validator :id="id">
+          <fieldset>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>服务分类
+              </label>
+              <div class="am-u-sm-3 am-u-end input-field">
+                <select2 required v-model="formData.categoryId" :options="category">
+                  <option value="">请选择</option>
+                </select2>
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>服务名称
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text" class="am-form-field" placeholder="请输入服务名称" required v-model="formData.productName">
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>单价
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text" class="am-form-field" placeholder="请输入商品价格" required v-model="formData.price">
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>单位
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <input type="text" class="am-form-field" placeholder="请输入商品单位" required v-model="formData.unit">
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                <span class="am-text-danger am-margin-right-xs am-text-xs"></span>上传图片
+              </label>
+              <div class="am-u-sm-9">
+                <button type="button" class="am-btn am-btn-default am-btn-success  am-form-file">
+                  <input id="uploadFile" @change="uploadImage" type="file" accept="application/formData">
+                  <span class="am-icon-cloud-upload"></span>请选择图片上传
+                </button>
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <label class="am-u-sm-3 am-form-label">
+                服务描述
+              </label>
+              <div class="am-u-sm-9 input-field">
+                <editor v-model="formData.content"></editor>
+              </div>
+            </div>
+
+            <div class="am-form-group">
+              <div class="am-u-sm-9 am-u-sm-push-3">
+                  <button type="submit" class="am-btn am-btn-primary">保存</button>
+              </div>
+            </div>
+
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import io from '../../lib/io'
+  import util from '../../lib/util'
+  export default{
+    data(){
+      return {
+        formData: {
+          productName: '',
+          content: '',
+          price: '',
+          unit: '',
+          categoryId:'',
+        },
+        category:[],
+        imgId:''
+      }
+    },
+    created: function () {
+      var productId = this.$params('productId');
+      if (productId) {
+        var _this = this
+        io.post(io.apiAdminServiceProductDetail, {productId: productId},
+          function (ret) {
+            if (ret.success) {
+              _this.formData = ret.data
+            }
+          },
+          function () {
+            _this.$alert('请求服务器失败')
+          })
+      }
+      this.loadCategoryData()
+    },
+    mounted:function(){
+      var _this = this ;
+      $('#' + this.id ).validator({
+        submit:function(e){
+          e.preventDefault();
+          var $submitBtn = $('button[type=submit]',e.target);
+          $submitBtn.attr("disabled" ,"disabled" )
+          _this.$showLoading()
+          var formValidity = this.isFormValid();
+          var complete = function(){
+            _this.$hiddenLoading()
+            $submitBtn.removeAttr("disabled" ,"disabled" )
+          }
+          if(formValidity){
+            _this.save(complete);
+          }else{
+            complete.call()
+          }
+        }
+      });
+    },
+    methods: {
+      save:function (complete) {
+        var _this = this
+        _this.formData.imageId = _this.imgId;
+        var data = _this.formData;
+        io.post(io.apiAdminSaveServiceProduct, $.extend({},data),
+          function (ret) {
+            complete.call();
+            if (ret.success) {
+              _this.$toast('OK')
+              _this.$router.push('/main/tradingService/rent/list')
+            } else {
+              _this.$alert(ret.desc)
+            }
+          },
+          function () {
+            complete.call();
+            _this.$alert('请求服务器失败')
+          })
+      },
+      loadCategoryData: function () {
+        var _this = this
+        io.post(io.apiAdminGetAllCategoryDetail, {type:1}, function (ret) {
+          if (ret.success) {
+            _this.category = ret.data.map(function (item) {
+              return {value: item.categoryId, text: item.name}
+            })
+          } else {
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+      uploadImage:function () {
+        var _this = this;
+        var formUrl = new FormData();
+        formUrl.append("file",document.getElementById('uploadFile').files[0]);
+        io.postUploadFile(io.apiAdminUploadFile,formUrl,function (ret) {
+          if (ret.data.success){
+            _this.imgId = ret.data.data
+          }else {
+            _this.$alert(ret.desc)
+          }
+        })
+      }
+    }
+  }
+</script>
---
+++ src/components/tradingService/ServiceRentList.vue
@@ -0,0 +1,177 @@
+<template>
+  <div >
+    <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
+      <div class="widget am-cf">
+        <div class="widget-head am-cf">
+          <div class="widget-title  am-cf">服务列表</div>
+        </div>
+        <div class="widget-body  am-fr">
+
+          <div class="am-u-sm-12 am-form ">
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group tpl-table-list-select">
+                <div class="am-form-group">
+                  <select2  required v-model="query.categoryId" :options="category">
+                    <option value="">服务分类</option>
+                  </select2>
+                </div>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <input type="text" class="am-input-lg" name="productName" v-model="query.productName" placeholder="请输入服务名称"/>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <button type="button" class="am-btn am-btn-default am-btn-success"
+                        @click="search" ><span class="am-icon-search"></span>查询
+                </button>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <button type="button" class="am-btn am-btn-default am-btn-success" @click="$router.push('/main/tradingService/service/add')">新增</button>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="am-u-sm-12 am-scrollable-horizontal">
+            <table width="100%" class="am-table am-table-bordered am-table-compact am-table-striped am-text-nowrap">
+              <thead>
+              <tr>
+                <th>序号</th>
+                <th>标题</th>
+                <th>服务类型</th>
+                <th>单价</th>
+                <th>单位</th>
+                <th>操作人</th>
+                <th>更新时间</th>
+                <th>操作</th>
+              </tr>
+              </thead>
+              <tbody>
+
+              <tr v-for="(item,index) in tableData" :key="item.productId">
+                <td>{{index+1}}</td>
+                <td>{{item.productName}}</td>
+                <td>{{item.categoryName}}</td>
+                <td>{{item.price}}</td>
+                <td>{{item.unit}}</td>
+                <td>{{item.username}}</td>
+                <td>{{item.updateTime | formatTime}}</td>
+                <td>
+                  <div class="tpl-table-black-operation">
+                    <a href="javascript:;" @click="$router.push('/main/tradingService/rent/edit/'+item.productId)" v-if="hasPermission('edit')">
+                      <i class="am-icon-edit"></i> 编辑
+                    </a>
+                    <a href="javascript:;" @click="deleteServiceProduct(item.productId)">
+                      <i class="am-icon-remove"></i>删除
+                    </a>
+                  </div>
+                </td>
+              </tr>
+
+              </tbody>
+            </table>
+          </div>
+          <div class="am-u-lg-12 am-cf">
+            <div class="am-fr">
+              <pagination v-bind:total="total" v-bind:pageNo="pageNo" v-bind:pageSize="pageSize" @paging="loadTableData" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+  </div>
+</template>
+
+<script>
+  import io from '../../lib/io'
+
+  import Pagination from '../base/Pagination'
+
+  export default{
+    data:function(){
+      return {
+        tableData:[],
+        total:0,
+        pageSize:15,
+        pageNo:1,
+        query:{
+          categoryId:'',
+          productName:'',
+        },
+        category:[],
+        searchConfig:{}
+      }
+    },
+    components: {
+      Pagination,
+    },
+    mounted:function(){
+      $(window).smoothScroll()
+    },
+    created:function(){
+      this.loadTableData(this.pageNo);
+      this.loadCategoryData();
+    },
+    methods:{
+      search:function(){
+        this.loadTableData()
+      },
+      loadCategoryData:function() {
+        var _this = this
+        io.post(io.apiAdminGetAllCategoryDetail, {type:1}, function (ret) {
+          if (ret.success) {
+            _this.category = ret.data.map(function (item) {
+              return {value: item.categoryId, text: item.name}
+            })
+          } else {
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+      loadTableData:function(pageNo){
+        var _this = this
+        _this.pageNo = pageNo || _this.pageNo || 1
+        io.post(io.apiAdminServiceProductList,$.extend({
+          pageNo:_this.pageNo,
+          pageSize:_this.pageSize,
+          type:1
+        },_this.query),function(ret){
+          if(ret.success){
+            _this.total = ret.data.total
+            _this.tableData = ret.data.list
+          }else{
+            _this.$alert(ret.desc)
+          }
+        })
+      },
+      deleteServiceProduct:function (productId) {
+        var _this = this ;
+        _this.productId = productId
+        _this.$confirm('你确定要删除？' ,
+          function(){
+            io.post(io.apiAdminDeleteServiceProduct,{productId:_this.productId},function(ret){
+              if(ret.success){
+                _this.$toast('OK')
+                _this.loadTableData()
+              }else{
+                _this.$alert(ret.desc)
+              }
+            })
+          });
+      },
+    }
+  }
+</script>
