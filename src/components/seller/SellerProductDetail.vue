f61427a7112b3ff1b331099786e6a3f71cc81e0b
zetao <zetao@hboxs.com>
2017-04-21 18:37:44 +0800

    fix

--- src/components/seller/SellerProductDetail.vue
+++ src/components/seller/SellerProductDetail.vue
@@ -11,30 +11,30 @@
 
         <table width="100%" class="am-table am-table-bordered am-table-compact">
           <tbody>
-            <tr>
-              <th>收货信息：</th>
-              <th></th>
-            </tr>
-            <tr>
-              <td class="bgColor">收货人：</td>
-              <td>{{order.consignee}}</td>
-              <td class="bgColor">手机号：</td>
-              <td>{{order.phoneNo}}</td>
-            </tr>
-            <tr>
-              <td class="bgColor">地址：</td>
-              <td>{{order.address}}</td>
-              <td class="bgColor">订单编号：</td>
-              <td>{{order.sn}}</td>
-            </tr>
-            <tr>
-              <td class="bgColor"> 用户：</td>
-              <td>{{order.userName}}</td>
-              <td class="bgColor">订单状态：</td>
-              <td>
-                {{order.status==0?'未支付':(order.status==1?'已支付':(order.status==2?'取消订单':'退费中的订单'))}}
-              </td>
-            </tr>
+          <tr>
+            <th>收货信息：</th>
+            <th></th>
+          </tr>
+          <tr>
+            <td class="bgColor">收货人：</td>
+            <td>{{order.consignee}}</td>
+            <td class="bgColor">手机号：</td>
+            <td>{{order.phoneNo}}</td>
+          </tr>
+          <tr>
+            <td class="bgColor">地址：</td>
+            <td>{{order.address}}</td>
+            <td class="bgColor">订单编号：</td>
+            <td>{{order.sn}}</td>
+          </tr>
+          <tr>
+            <td class="bgColor"> 用户：</td>
+            <td>{{order.userName}}</td>
+            <td class="bgColor">订单状态：</td>
+            <td>
+              {{order.status==0?'未支付':(order.status==1?'已支付':(order.status==2?'取消订单':'退费中的订单'))}}
+            </td>
+          </tr>
           </tbody>
         </table>
 
@@ -69,32 +69,31 @@
         <div class="am-u-sm-12">
           <div class="am-panel am-panel-default">
             <div class="am-panel-hd"><label>评论</label></div>
-            <div class="am-panel-bd">
-              <ul class="am-comments-list am-comments-list-flip">
-                <li class="am-comment" v-for="item in commentData" v-if="commentData!=''" :key="item.commentId">
-                  <div class="am-panel am-panel-default">
-                    <div class="am-panel-hd">
-                      <label>
-                        <el-rate v-model="item.evaluation" disabled ></el-rate>
-                      </label>
-                      <span class="left-margin">{{item.createTime | formatDate}}</span>
-                      <span class="left-margin">买家：{{item.userName}}</span>
-                    </div>
-                    <div class="am-panel-bd">
-                      <article>{{item.comment}}</article>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-
-              <div class="am-u-lg-12 am-cf">
-                <div class="am-fr">
-                  <pagination v-bind:total="total" v-bind:pageNo="pageNo" v-bind:pageSize="pageSize" @paging="loadTableData"/>
+          </div>
+          <ul class="am-comments-list am-comments-list-flip">
+            <li class="am-comment" v-for="item in commentData" v-if="commentData!=''" :key="item.commentId">
+              <div class="am-panel am-panel-default">
+                <div class="am-panel-hd">
+                  <label>
+                    <el-rate v-model="item.evaluation" disabled></el-rate>
+                  </label>
+                  <span class="left-margin">{{item.createTime | formatTime}}</span>
+                  <span class="left-margin">买家：{{item.userName}}</span>
+                </div>
+                <div class="am-panel-bd">
+                  <article>{{item.comment}}</article>
                 </div>
               </div>
+            </li>
+          </ul>
 
+          <div class="am-u-lg-12 am-cf">
+            <div class="am-fr">
+              <pagination v-bind:total="total" v-bind:pageNo="pageNo" v-bind:pageSize="pageSize"
+                          @paging="loadTableData"/>
             </div>
           </div>
+
         </div>
 
       </div>
@@ -103,7 +102,7 @@
 </template>
 
 <style>
-  .left-margin{
+  .left-margin {
     margin-left: 5%;
   }
 </style>
@@ -118,14 +117,14 @@
       return {
         tableData: [],
         orderItemId: '',
-        order:[],
-        pageNo:1,
-        pageSize:5,
-        total:0,
-        commentData:[],
+        order: [],
+        pageNo: 1,
+        pageSize: 5,
+        total: 0,
+        commentData: [],
       }
     },
-    components:{
+    components: {
       Pagination
     },
     mounted: function () {
@@ -153,15 +152,15 @@
         var _this = this
         _this.pageNo = pageNo || _this.pageNo || 1
         _this.productId = _this.tableData.productId
-        io.post(io.apiAdminCommentList,{
-          productId:_this.productId,
-          pageNo:_this.pageNo,
-          pageSize:_this.pageSize
-        },function (ret) {
-          if (ret.success){
+        io.post(io.apiAdminCommentList, {
+          productId: _this.productId,
+          pageNo: _this.pageNo,
+          pageSize: _this.pageSize
+        }, function (ret) {
+          if (ret.success) {
             _this.commentData = ret.data.list
             _this.total = ret.data.total
-          }else {
+          } else {
             _this.$alert(ret.desc)
           }
         })
