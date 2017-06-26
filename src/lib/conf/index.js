d30e7fe77cf53efb415e570165b668a97c434c1c
zhyc <731692254@qq.com>
2017-06-26 09:49:14 +0800

    pull

--- src/lib/conf/index.js
+++ src/lib/conf/index.js
@@ -4,7 +4,7 @@ window.UEDITOR_HOME_URL = '/static/ueditor/'
 
 export default {
 
-  baseApiPath : 'development' == process.env.NODE_ENV ? 'http://localhost:8080' : 'http://test.yuyou100.com/yyfn-api',
+  baseApiPath : 'development' == process.env.NODE_ENV ? 'http://test.yuyou100.com/yyfn-api' : 'http://test.yuyou100.com/yyfn-api',
   basePath : 'development' == process.env.NODE_ENV ? 'http://dev.yuyou100.com:7070' : 'http://test.yuyou100.com',
   qiniuCDN : 'http://static.yuyou100.com/',
   qiniuUploadTokenApi: 'http://test.yuyou100.com/yyfn-api/api/qiniu/upload/token'
