f4b05bc5377989977bdf43590efed598fee48e2d
温佐镜 <wenzuojing@gmail.com>
2017-08-31 10:08:06 +0800

    fix bug

--- src/components/enroll/StudentRegList.vue
+++ src/components/enroll/StudentRegList.vue
@@ -3,41 +3,80 @@
     <div class="am-u-sm-12 am-u-md-12 am-u-lg-12">
       <div class="widget am-cf">
         <div class="widget-head am-cf">
-          <div class="widget-title  am-cf">报名列表</div>
-          <div class="widget-function am-fr">
-            <button type="button" class="am-btn am-btn-default" @click="$router.go(-1)">返回</button>
-          </div>
+          <div class="widget-title  am-cf">班级列表</div>
         </div>
-        <div class="widget-body am-fr">
-
-          <!--searching condition-->
+        <div class="widget-body  am-fr">
           <div class="am-u-sm-12 am-form ">
-
             <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
               <div class="am-form-group">
-                <input type="text"  v-model="query.studentName" placeholder="请输入学生名"/>
+                <select2 v-model="query.areaTeamId" :options="areaTeams">
+                  <option value="">区域</option>
+                </select2>
+              </div>
+            </div>
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <select2 v-model="query.busTeamId" :options="busTeams">
+                  <option value="">业务组</option>
+                </select2>
               </div>
             </div>
 
             <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
               <div class="am-form-group">
-                <select2 required  v-model="query.regStatus"  >
-                  <option value="">状态</option>
+                <select2 v-model="query.periodId" :options="periods">
+                  <option value="">期数</option>
+                </select2>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <input type="text" name="className" v-model="query.className" placeholder="请输入班级名称"/>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <input type="text" name="teacherNames" v-model="query.studentName" placeholder="请输入学生姓名"/>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <select2 v-model="query.regStatus">
+                  <option value="">报名状态</option>
                   <option value="0">在读</option>
                   <option value="1">转班</option>
+                  <!--<option value="2">退账户</option>-->
                   <option value="3">退班退费</option>
-                  <option value="4">撤销</option>
+                  <option value="4">无效</option>
+                </select2>
+              </div>
+            </div>
+
+            <div class="am-u-sm-12 am-u-md-12 am-u-lg-3">
+              <div class="am-form-group">
+                <select2 v-model="query.chargingStatus">
+                  <option value="">缴费状态</option>
+                  <option value="0">未缴费</option>
+                  <option value="1">欠费</option>
+                  <option value="3">已缴费</option>
                 </select2>
               </div>
             </div>
 
             <div class="am-u-sm-12 am-u-md-12 am-u-lg-3 am-u-end">
               <div class="am-form-group">
-                <button type="button" class="am-btn am-btn-default am-btn-success "
-                        @click="search" ><span class="am-icon-search"></span>查询
-              </button>
+                <button type="button" class="am-btn am-btn-default am-btn-success"
+                        @click="search"><span class="am-icon-search"></span>查询
+                </button>
+                <button type="button" class="am-btn am-btn-default am-btn-success"
+                        @click="exportStudentReg"><span class="am-icon-download"></span>导出
+                </button>
               </div>
             </div>
+
 
           </div>
 
@@ -48,39 +87,120 @@
               stripe
               style="min-width: 100%">
               <el-table-column
-                prop="studentName"
-                label="姓名"
+                fixed
+                prop="studentReg.studentName"
+                label="学生姓名"
+                min-width="100">
+              </el-table-column>
+              <el-table-column
+                prop="studentReg.className"
+                label="班级名称"
                 min-width="200">
               </el-table-column>
               <el-table-column
-                prop="startAmount"
-                label="报读开始讲数"
-                min-width="150">
+                prop="courseClass.gradeName"
+                label="年级"
+                min-width="100">
               </el-table-column>
               <el-table-column
-                prop="endAmount"
-                label="报读结束讲数"
-                min-width="150">
+                prop="courseClass.subjectName"
+                label="科目"
+                min-width="100">
               </el-table-column>
               <el-table-column
-                label="报读日期"
-                min-width="150">
+                prop="courseClass.periodName"
+                label="期名"
+                min-width="100">
+              </el-table-column>
+              <el-table-column
+                prop="courseClass.campusName"
+                label="校区"
+                min-width="200">
+              </el-table-column>
+              <el-table-column
+                prop="courseClass.roomName"
+                label="教室"
+                min-width="100">
+              </el-table-column>
+              <el-table-column
+                label="众筹"
+                min-width="100">
                 <template scope="scope">
-                  {{ scope.row.createTime | formatDate }}
+                  {{scope.row.courseClass.classType == 0 ? "否" : "是"}}
                 </template>
               </el-table-column>
               <el-table-column
-                label="状态"
+                label="开课日期"
                 min-width="150">
                 <template scope="scope">
-                  {{scope.row.regStatus == 0 ? '在读' : scope.row.regStatus == 1 ? '转班' : scope.row.regStatus == 2 ? '退账户' :  scope.row.regStatus == 3 ? '退班退费' :'撤销' }}
+                  {{scope.row.courseClass.startCourseTime | formatDate}}
                 </template>
+              </el-table-column>
+              <el-table-column
+                prop="courseClass.studyingTime"
+                label="上课时间"
+                min-width="150">
+              </el-table-column>
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="教师"
+                min-width="100">
+              </el-table-column>
+
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="报读讲数"
+                min-width="100">
+                <template scope="scope">
+                {{scope.row.studentReg.startAmount}}~{{scope.row.studentReg.endAmount}}
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="应缴金额"
+                min-width="100">
+                <template scope="scope">
+                {{scope.row.studentReg.totalAmount}}
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="已缴金额"
+                min-width="100">
+                <template scope="scope">
+                {{scope.row.studentReg.payAmount}}
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="报名状态"
+                min-width="100">
+                <template scope="scope">
+                {{{'0': '在读', '1': '转班', '2': '退账户', '3': '退班退费', '4': '无效'}[scope.row.studentReg.regStatus]}}
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                prop="courseClass.teacherNames"
+                label="缴费状态"
+                min-width="100">
+                <template scope="scope">
+                {{{'0': '未缴费', '1': '欠费', '2': '已缴费'}[scope.row.studentReg.chargingStatus]}}
+                </template>
+              </el-table-column>
+
+              <el-table-column
+                prop="studentReg.operator"
+                label="操作人"
+                min-width="100">
               </el-table-column>
 
             </el-table>
           </div>
           <div class="am-u-lg-12 am-cf">
-
             <div class="am-fr">
               <pagination v-bind:total="total" v-bind:pageNo="pageNo" v-bind:pageSize="pageSize"
                           @paging="loadTableData"/>
@@ -90,26 +210,26 @@
         </div>
       </div>
     </div>
-
   </div>
 </template>
-
 <script>
   import io from '../../lib/io'
-
   import Pagination from '../base/Pagination'
 
-  export default{
+  export default {
     data: function () {
       return {
         tableData: [],
         total: 0,
-        pageSize: 20,
+        pageSize: 10,
         pageNo: 1,
         query: {
-          regStatus: '0',
-          classId: ''
-        }
+          areaTeamId: window.config.areaTeams[0] && window.config.areaTeams[0].areaTeamId || '',
+          busTeamId: '',
+          productId: '',
+          periodId: window.config.periods.filter(item => item.isCurrent == 1)[0].periodId,
+        },
+        searchConfig: {},
       }
     },
     components: {
@@ -119,13 +239,39 @@
       $(window).smoothScroll()
     },
     created: function () {
-      this.query.classId = this.$params('classId')
       this.loadTableData(this.pageNo)
+    },
+    computed: {
+      areaTeams: function () {
+        var options = ( this.$root.config.areaTeams || [] )
+          .map(function (item) {
+            return {value: item.areaTeamId, text: item.name}
+          })
+        return options
+      },
+      busTeams: function () {
+        var options = ( ( this.query.areaTeamId  ) ? ( this.$root.config.groupBusTeams[this.query.areaTeamId] || [] ) : [] )
+          .map(function (item) {
+            return {value: item.busTeamId, text: item.name}
+          })
+        this.query.busTeamId = ''
+        return options
+      },
+      periods: function () {
+        return this.$root.config.periods.map(function (item) {
+          return {value: item.periodId, text: item.periodName}
+        })
+      }
     },
     methods: {
       search: function () {
         this.loadTableData(1)
       },
+      //导出注册信息
+      exportStudentReg: function () {
+        io.downloadFile(io.apiAdminExportStudentReg, this.query)
+      },
+
       loadTableData: function (pageNo) {
         var _this = this
         _this.pageNo = pageNo || _this.pageNo || 1
@@ -140,8 +286,8 @@
             _this.$alert(ret.desc)
           }
         })
-      }
-
+      },
     }
   }
 </script>
+
