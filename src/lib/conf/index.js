dfa1271157a3c54b88fac26e7ad820b713ecc7a4
温佐镜 <wenzuojing@gmail.com>
2017-11-13 22:52:43 +0800

    fb

--- src/lib/conf/index.js
+++ src/lib/conf/index.js
@@ -1,8 +1,5 @@
-
-
 window.UEDITOR_HOME_URL = '/static/ueditor/'
 
-/**
 export default {
 
   baseApiPath : 'development' == process.env.NODE_ENV ? 'http://test.yuyou100.com/yyfn-api' : 'http://test.yuyou100.com/yyfn-api',
@@ -15,29 +12,3 @@ export default {
   qiniuUploadTokenApi: 'http://test.yuyou100.com/yyfn-api/api/qiniu/upload/token'
 
 }
-
- */
-
-
-
- //prod
-
- export default {
-  baseApiPath : 'http://b.yuyou100.com/yyfn-api',
-  basePath : 'http://b.yuyou100.com',
-  studentBasePath : 'http://s.y.yuyou100.com',
-  qiniuCDN : 'http://static.yuyou100.com/',
-  qiniuUploadTokenApi: 'http://b.yuyou100.com/yyfn-api/api/qiniu/upload/token'
-}
-
-
-/*
- export default {
-
- baseApiPath : 'development' == process.env.NODE_ENV ? 'http://test.yuyou100.com/yyfn-api' : 'http://test.yuyou100.com/yyfn-api',
- basePath : 'development' == process.env.NODE_ENV ? 'http://dev.yuyou100.com:7070' : 'http://test.yuyou100.com',
- studentBasePath : 'development' == process.env.NODE_ENV ? 'http://s.test.yuyou100.com' : 'http://s.test.yuyou100.com',
- qiniuCDN : 'http://static.yuyou100.com/',
- qiniuUploadTokenApi: 'http://test.yuyou100.com/yyfn-api/api/qiniu/upload/token'
-
- }*/
