c196094f53f9c0fdb83a0d4b0685e7266bb2ef07
shuizheng <1941172562@qq.com>
2017-12-11 15:46:14 +0800

    'bugfix'

--- src/lib/conf/index.js
+++ src/lib/conf/index.js
@@ -1,9 +1,5 @@
+import prod from './index_prod'
+import test from './index_test'
 window.UEDITOR_HOME_URL = '/static/ueditor/'
 
-export default {
-  baseApiPath : 'http://b.yuyou100.com/yyfn-api',
-  basePath : 'http://b.yuyou100.com',
-  studentBasePath : 'http://s.y.yuyou100.com',
-  qiniuCDN : 'http://static.yuyou100.com/',
-  qiniuUploadTokenApi: 'http://b.yuyou100.com/yyfn-api/api/qiniu/upload/token'
-}
+export default 'development' == process.env.NODE_ENV ? test : prod
