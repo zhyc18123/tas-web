2fa18cefe1857e41af634ebdfbbb5a1384c885fc
zhyc <731692254@qq.com>
2018-05-11 09:49:59 +0800

    up

--- src/lib/conf/index.js
+++
@@ -1,4 +0,0 @@
-import prod from './index_prod'
-import test from './index_test'
-
-export default 'development' == process.env.NODE_ENV ? test : prod
--- src/lib/conf/index_prod.js
+++
@@ -1,8 +0,0 @@
-window.UEDITOR_HOME_URL = '/static/ueditor/'
-export default {
-  baseApiPath : 'http://b.yuyou100.com/yyfn-api',
-  basePath : 'http://b.yuyou100.com',
-  studentBasePath : 'http://s.y.yuyou100.com',
-  qiniuCDN : 'http://static.yuyou100.com/',
-  qiniuUploadTokenApi: 'http://b.yuyou100.com/yyfn-api/api/qiniu/upload/token'
-}
