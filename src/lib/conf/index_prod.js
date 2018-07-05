f0c7adf235ff4ef52d16b459502ce3557ae37280
zhyc <731692254@qq.com>
2018-07-05 11:39:06 +0800

    up

--- src/lib/conf/index_prod.js
+++ src/lib/conf/index_prod.js
@@ -2,8 +2,8 @@
 window.UEDITOR_HOME_URL = '/static/ueditor/'
 
 export default {
-  baseApiPath : 'http://trp.yuyou100.com/trp-api',
-  basePath : 'http://trp.yuyou100.com',
+  baseApiPath : 'http://work.yuyou100.com/trp-api',
+  basePath : 'http://work.yuyou100.com.com',
   qiniuCDN : 'http://static.yuyou100.com/',
   qiniuUploadTokenApi: 'http://b.yuyou100.com/yyfn-api/api/qiniu/upload/token'
 }
