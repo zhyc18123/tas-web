72a5e4f71c89bd8a0bedae8810696846c8fdcd6c
zhyc <731692254@qq.com>
2018-07-05 14:55:55 +0800

    up

--- src/lib/conf/index_prod.js
+++ src/lib/conf/index_prod.js
@@ -3,7 +3,7 @@ window.UEDITOR_HOME_URL = '/static/ueditor/'
 
 export default {
   baseApiPath : 'http://work.yuyou100.com/tas-gateway',
-  basePath : 'http://work.yuyou100.com.com',
+  basePath : 'http://work.yuyou100.com',
   qiniuCDN : 'http://static.yuyou100.com/',
   qiniuUploadTokenApi: 'http://b.yuyou100.com/yyfn-api/api/qiniu/upload/token'
 }
