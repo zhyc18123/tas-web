1dc9f2641e5546e0408a6428d77ba7756ccbe9fa
jiangwei <1041366803@qq.com>
2018-07-18 19:11:45 +0800

    修改

--- src/components/system/class/ClassList.vue
+++ src/components/system/class/ClassList.vue
@@ -50,7 +50,7 @@
             <el-table-column prop="baseLevelName" label="班型"  align="center"></el-table-column>
             <el-table-column  label="开课状态" align="center">
                 <template scope="scope">
-                    {{scope.row.status===0?'未开课':scope.row.status===1?'已开课':'已结课'}}
+                    {{scope.row.status===0?'未开课':scope.row.status===1?'开课中':'已结课'}}
                 </template>
                  </el-table-column>
             <el-table-column prop="section"  label="年级" align="center"> </el-table-column>
